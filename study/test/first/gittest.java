9b6ba98cf424b4a9f2f7b79c96b977a3cd191b85
zeta2017827 <2315815073@qq.com>
2023-03-11 00:59:11 +0800

    first commit test2

--- study/test/first/gittest.java
+++ study/test/first/gittest.java
@@ -3,6 +3,8 @@ package study.test.first;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hellow git ~");
+        System.out.println("let me do it fot you ~");
+
 
     }
 }
