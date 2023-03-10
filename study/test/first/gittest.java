41b377d9757dcd1acb92f2a5e93656d09a7bf7da
zeta2017827 <2315815073@qq.com>
2023-03-11 01:17:20 +0800

    hot-fix commit test3

--- study/test/first/gittest.java
+++ study/test/first/gittest.java
@@ -1,10 +1,13 @@
 package study.test.first;
 
+import com.sun.tools.javac.util.BasicDiagnosticFormatter;
+
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hellow git ~");
         System.out.println("let me do it fot you ~");
         System.out.println("chould me ~");
+        System.out.println("what wrong with you, bro?");
 
 
     }
